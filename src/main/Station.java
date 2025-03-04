b1a21a0396027943167b6c78c8edae1b9950f4d2
LiamRittenburg <138069039+LiamRittenburg@users.noreply.github.com>
2025-03-04 17:20:57 +0000

    Subm

--- src/main/Station.java
+++ src/main/Station.java
@@ -6,46 +6,128 @@ public class Station {
 	public Queue<Rider> southBoundRiders;
 	public Queue<Train> northBoundTrains;
 	public Queue<Train> southBoundTrains;
+	private String name;
 	
 	public Station(String name) {
-		
+		northBoundRiders = new Queue(20);
+		southBoundRiders = new Queue(20);
+		northBoundTrains = new Queue(20);
+		southBoundTrains = new Queue(20);
+		this.name = name;
 	}
 	
 	public boolean addRider(Rider r) { 
-		return false;
+		if(r.getStarting().equals(stationName()))
+		{
+			if(r.goingNorth())
+			{
+				northBoundRiders.enqueue(r);
+			}
+			else
+			{
+				southBoundRiders.enqueue(r);
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
 	public String addTrain(Train t) {
-		return null;
+		String enter = stationName() + " Disembarking Passengers:\n";
+		t.updateStation(stationName);
+		enter = enter + t.disembarkPassengers() + "\n";
+		if(t.goingNorth())
+		{
+			northBoundTrains.enqueue(t);
+		}
+		else
+		{
+			southBoundTrains.enqueue(t);
+		}
 	}
 	
-	public Train southBoardTrain() {
-		return null;
+	public Train southBoardTrain() 
+	{
+		Train t = null;
+		if(southBoundTrains.size() > 0)
+		{
+			t = southBoundTrains.front();
+			southBoundTrains.dequeue();
+			while(t.hasSpaceForPassengers() && southBoundRiders.size() > 0)
+			{
+				Rider r = southBoundRiders.front();
+				southBoundRiders.dequeue();
+				t.addPassenger(r);
+			}
+		}
+		return t;
 	}
 	
-	public Train northBoardTrain() {
-		return null;
+	public Train northBoardTrain() 
+	{
+		Train t = null;
+		if(northBoundTrains.size() > 0)
+		{
+			t = northBoundTrains.front();
+			northBoundTrains.dequeue();
+			while(t.hasSpaceForPassengers() && northBoundRiders.size() > 0)
+			{
+				Rider r = northBoundRiders.front();
+				northBoundRiders.dequeue();
+				t.addPassenger(r);
+			}
+		}
+		return t;
 	}
 	
 	public void moveTrainNorthToSouth() {
-		
+		if(northBoundTrains.size() > 0)
+		{
+			Train t = northBoundTrains.front();
+			t.swapDirection();
+			northBoundTrains.dequeue();
+			southBoundTrains.enqueue(t);
+		}
+
 	}
 	
 	public void moveTrainSouthToNorth() {
-		
+		if(southBoundTrains.size() > 0)
+		{
+			Train t = southBoundTrains.front();
+			t.swapDirection();
+			southBoundTrains.dequeue();
+			northBoundTrains.enqueue(t);
+		}
 	}
 	
 	@Override
 	public String toString() {
-		return null;
+		String station = "Station: " + stationName() + "\n";
+		station = station + northBoundTrains.size() + " north-bound trains waiting\n";
+		station = station + southBoundTrains.size() + " south-bound trains waiting\n";
+		station = station + northBoundRiders.size() + " north-bound passengers waiting\n";
+		station = station + southBoundRiders.size() + " south-bound passengers waiting\n";
+		return station;
 	}
 	
 	public String stationName() {
-		return null;
+		return this.name;
 	}
 	
 	@Override
 	public boolean equals(Object o) {
-		return false;
+		if (o instanceof String)
+		{
+			String other = (String) o;
+			return other.equals(o);
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
