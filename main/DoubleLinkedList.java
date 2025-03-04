d5ba6c3c86df74ddf6a623fbe7a0dc1add7a674b
LiamRittenburg <138069039+LiamRittenburg@users.noreply.github.com>
2025-03-03 22:08:41 -0500

    Add files via upload

---
+++ main/DoubleLinkedList.java
@@ -0,0 +1,34 @@
+package main;
+
+public class DoubleLinkedList<T> {
+
+	public DoubleLinkedList() {
+		
+	}
+	
+	public Node<T> getFirst() {
+		return null;
+	}
+	
+	
+	public void insert(T element) {
+		
+	}
+	
+	public T delete(T key) {
+		return null;
+	}
+	
+	public T get(T key) {
+		return null;
+	}
+	
+	public int size() {
+		return -1;
+	}
+	
+	@Override
+	public String toString() {
+		return null;
+	}
+}
---
+++ main/Train.java
@@ -0,0 +1,49 @@
+package main;
+
+public class Train {
+
+	public static final int TOTAL_PASSENGERS = 10;
+	public Rider[] passengers;
+	public int passengerIndex;
+	
+	public Train(String currentStation, int direction) {
+		
+	}
+	
+	public boolean goingNorth() {
+		return false;
+	}
+	
+	public void swapDirection() {
+		
+	}
+	
+	public String currentPassengers() {
+		return null;
+	}
+	
+	public boolean addPassenger(Rider r) {
+		return false;
+	}
+	
+	public boolean hasSpaceForPassengers() {
+		return false;
+	}
+	
+	public String disembarkPassengers() {
+		return null;
+	}
+	
+	public void updateStation(String s) {
+		
+	}
+	
+	public String getStation() {
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return null;
+	}
+}
